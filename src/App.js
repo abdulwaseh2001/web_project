8d0bb4174409c4c194f33c9ca06e5bc74812335c
abdulwaseh2001 <abdulwasayzafar2001@gmail.com>
2025-04-24 22:27:57 +0500

    2nd commit with frontend setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Marketplace from './pages/Marketplace';
+import NotFound from './pages/NotFound';
+
+
+
+
+
 function App() {
-  return <h1>The Indie Coalition</h1>;
+  return (
+    <Router>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
