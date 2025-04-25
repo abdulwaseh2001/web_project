4efa5200a5d4f466182de095a43a383003016f85
abdulwaseh2001 <abdulwasayzafar2001@gmail.com>
2025-04-25 06:45:33 +0500

    added login and registratione

---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+return <div classname = "p-6 text-2xl">welcome to indie coaltion</div>
+
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,4 @@
+export default function NotFound() {
+return <div classname = "p-6 text-x1">404- page not NotFound  </div>
+
+}
