c2be9c47794231b2976d43d1687851112e2cc791
abdulwaseh2001 <abdulwasayzafar2001@gmail.com>
2025-04-25 06:05:14 +0500

    added dark mode

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import react from 'react'
+import { link } from 'react-router-dom'
+import DarkModeToggle from './DarkModeToggle'
+
+export default function Navbar()
+{
+    return(
+        <nav classname ="flex justify-between p-4 bg-gray-100 dark:bg-gray-800">
+            <div classname = "space-x-4">
+                <Link to="/">Home</Link>
+                <Link to="/marketplace">Marketplace</Link>
+                <Link to="/profile">Profile</Link>
+
+            </div>
+            <div classname = "space-x-4">
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+                <DarkModeToggle />
+            </div>
+        </nav>
+
+
+    );
+
+
+
+
+
+
+
+
+
+}
